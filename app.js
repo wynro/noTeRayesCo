74fbdf49ce7baa5883c2de1a87af75e92efd7686
Guillermo Robles <guillerobles1995@gmail.com>
2017-08-20 01:19:13 +0200

    Remove database dependency

--- app.js
+++ app.js
@@ -1,45 +1,26 @@
 var express = require('express');
-var mysql = require('mysql');
+var bodyParser = require('body-parser');
+
+var nVisits = 0;
 
 var app = express();
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
-var connection = mysql.createConnection({
-  host     : process.env.MYSQL_HOST,
-  port     : process.env.MYSQL_PORT,
-  user     : process.env.MYSQL_USER,
-  password : process.env.MYSQL_PASS,
-  database : process.env.MYSQL_DB
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
+app.post('/setVisits', function(req,res) {
+    var visits = req.body.value;
+    if (visits) {
+        nVisits = visits;
+    }
 });
 
 app.get('/getVisits', function(req,res){
     var nVisits = 0;
-    connection.query('SELECT value FROM stats WHERE stats.id_stat=\'visits\'', function (error, results, fields) {
-        if (!error) {
-            console.log(results[0].value);
-            nVisits = results[0].value + 1;
-            res.send(JSON.stringify(nVisits));
-            var sql = 'UPDATE stats SET value = value + 1 WHERE id_stat=\'visits\'';
-            connection.query(sql, function (error, results, fields) {
-                if (error) {
-                    console.error("Couldn't update visits from databse.");
-                }
-            });
-        } else {
-            nVisits = 'unos cuantos'
-            res.send(JSON.stringify(nVisits));
-            console.error("Couldn't retrieve visits from databse.");
-        }
-        
-    });
+    if (nVisits) {
+        res.send(JSON.stringify(nVisits));
+    } else {
+        res.send(JSON.stringify('unos cuantos'));
+    }
 });
 
 var server = app.listen(process.env.PORT || 8080, function(){
